e8179cd6d66ba00cbd6f8b0249cab8b2372fdfae
Aayushraj1 <50690531+Aayushraj1@users.noreply.github.com>
2026-08-05 19:41:02 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,24 @@
 terraform {
+
   required_version = ">= 1.6.0"
+
+  required_providers {
+
+    azurerm = {
+
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+
+    }
+
+  }
+
 }
 
-provider "local" {}
+provider "azurerm" {
+
+  features {}
+
+  subscription_id = var.subscription_id
+
+}
